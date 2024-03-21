260bf3598e02efdeee3af2d5807fdd2708b3c54c
aliOrifjonovich <orifjonovichalisher@gmail.com>
2024-03-21 21:12:24 +0500

    added all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { TableComponent } from './components/Table/table.component';
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    HeaderComponent,
+    TableComponent,
+    FormsModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    FooterComponent,
+    ReactiveFormsModule,
+  ],
 })
 export class AppComponent {
-  title = 'wadFrontend14669';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ParticipantsComponent } from './pages/participants/participants.component';
+import { EventsComponent } from './pages/events/events.component';
+import { CreateEventsComponent } from './pages/create-events/create-events.component';
+import { CreateParticipantsComponent } from './pages/create-participants/create-participants.component';
+import { EventsDetailViewComponent } from './pages/events-detail-view/events-detail-view.component';
+import { ParticipantsDetailViewComponent } from './pages/participants-detail-view/participants-detail-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'participants',
+    component: ParticipantsComponent,
+  },
+  {
+    path: 'events',
+    component: EventsComponent,
+  },
+  {
+    path: 'create-events',
+    component: CreateEventsComponent,
+  },
+  {
+    path: 'create-participants',
+    component: CreateParticipantsComponent,
+  },
+  {
+    path: 'events/:id',
+    component: EventsDetailViewComponent,
+  },
+  {
+    path: 'participants/:id',
+    component: ParticipantsDetailViewComponent,
+  },
+];
